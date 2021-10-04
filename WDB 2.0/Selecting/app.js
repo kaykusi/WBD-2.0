caea3da935b717b32361f1d41478209cc505d635
kaykusi <therealkusi@gmail.com>
2021-10-04 08:53:36 +0000

    new changes

---
+++ WDB 2.0/Selecting/app.js
@@ -0,0 +1,5 @@
+const allImages = document.getElementsByTagName("img");
+
+for (let img of allImages) {
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Love_the_socks_on_this_Silkie_hen_%2816443981425%29.jpg/640px-Love_the_socks_on_this_Silkie_hen_%2816443981425%29.jpg"
+}
